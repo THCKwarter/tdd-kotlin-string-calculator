800ffac3e73961f95305609f9b8f14e3c9be0143
THCKwarter <MattsMisc.Items@yahoo.com>
2019-05-23 14:21:59 -0500

    Started adding TS template

--- kotlin/src/test/StringCalculatorUnitTest.kt
+++ kotlin/src/test/StringCalculatorUnitTest.kt
@@ -1,6 +1,8 @@
 package test
 
 import main.StringCalculator
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class StringCalculatorUnitTest {
     private val sc = StringCalculator()
